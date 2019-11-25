df00145150d02a9718935a89677685cdc6e1291c
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2019-11-24 23:14:56 -0500

    Move model API ffi to codegen

--- src/rust/write.rs
+++ src/rust/write.rs
@@ -15,134 +15,9 @@ pub(super) fn write_rust_interface_object(r: &mut Vec<u8>, object: &Object) -> R
     c_ffi::push_clear(&mut scope, object);
     c_ffi::push_functions(&mut scope, object);
     c_ffi::push_properties(&mut scope, object);
+    c_ffi::push_models(&mut scope, object);
 
     writeln!(r, "{}", scope.to_string())?;
-
-    if object.object_type == ObjectType::List {
-        writeln!(
-            r,
-            "
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_row_count(ptr: *const {0}) -> c_int {{
-    to_c_int((&*ptr).row_count())
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_insert_rows(ptr: *mut {0}, row: c_int, count: c_int) -> bool {{
-    match (to_usize(row), to_usize(count)) {{
-        (Some(row), Some(count)) => {{
-            (&mut *ptr).insert_rows(row, count)
-        }}
-        _ => false
-    }}
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_remove_rows(ptr: *mut {0}, row: c_int, count: c_int) -> bool {{
-    match (to_usize(row), to_usize(count)) {{
-        (Some(row), Some(count)) => {{
-            (&mut *ptr).remove_rows(row, count)
-        }}
-        _ => false
-    }}
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_can_fetch_more(ptr: *const {0}) -> bool {{
-    (&*ptr).can_fetch_more()
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_fetch_more(ptr: *mut {0}) {{
-    (&mut *ptr).fetch_more()
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_sort(
-    ptr: *mut {0},
-    column: u8,
-    order: SortOrder,
-) {{
-    (&mut *ptr).sort(column, order)
-}}",
-            object.name, lcname
-        )?;
-    } else if object.object_type == ObjectType::Tree {
-        writeln!(
-            r,
-            "
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_row_count(
-    ptr: *const {0},
-    index: COption<usize>,
-) -> c_int {{
-    to_c_int((&*ptr).row_count(index.into()))
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_can_fetch_more(
-    ptr: *const {0},
-    index: COption<usize>,
-) -> bool {{
-    (&*ptr).can_fetch_more(index.into())
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_fetch_more(ptr: *mut {0}, index: COption<usize>) {{
-    (&mut *ptr).fetch_more(index.into())
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_sort(
-    ptr: *mut {0},
-    column: u8,
-    order: SortOrder
-) {{
-    (&mut *ptr).sort(column, order)
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_check_row(
-    ptr: *const {0},
-    index: usize,
-    row: c_int,
-) -> COption<usize> {{
-    match to_usize(row) {{
-        Some(row) => (&*ptr).check_row(index, row).into(),
-        other => other.into()
-    }}
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_index(
-    ptr: *const {0},
-    index: COption<usize>,
-    row: c_int,
-) -> usize {{
-    (&*ptr).index(index.into(), to_usize(row).unwrap_or(0))
-}}
-
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_parent(ptr: *const {0}, index: usize) -> QModelIndex {{
-    if let Some(parent) = (&*ptr).parent(index) {{
-        QModelIndex {{
-            row: to_c_int((&*ptr).row(parent)),
-            internal_id: parent,
-        }}
-    }} else {{
-        QModelIndex {{
-            row: -1,
-            internal_id: 0,
-        }}
-    }}
-}}
-#[no_mangle]
-pub unsafe extern \"C\" fn {1}_row(ptr: *const {0}, index: usize) -> c_int {{
-    to_c_int((&*ptr).row(index))
-}}",
-            object.name, lcname
-        )?;
-    }
 
     if object.object_type != ObjectType::Object {
         let (index_decl, index) = if object.object_type == ObjectType::Tree {
@@ -150,6 +25,7 @@ pub unsafe extern \"C\" fn {1}_row(ptr: *const {0}, index: usize) -> c_int {{
         } else {
             (", row: c_int", "to_usize(row).unwrap_or(0)")
         };
+
         for (name, ip) in &object.item_properties {
             if ip.is_complex() && !ip.optional {
                 writeln!(
